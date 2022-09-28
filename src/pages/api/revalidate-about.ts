9977cbfbe8c38b3773de9911a80c9fe35639e777
David Nicholas <Dnicholas@bitovi.com>
2022-09-28 15:55:19 -0700

    revalidate

---
+++ src/pages/api/revalidate-about.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.query.revalidationSecret !== process.env.REVALIDATION_SECRET) {
+    return res.status(401).json({ message: "Invalid secret" });
+  }
+
+  try {
+    await res.revalidate("/about");
+
+    return res.json({ revalidated: true });
+  } catch (error) {
+    // Just like before, if an error happens, the cached page remains
+    return res.status(500).send("Revalidation errror: " + error);
+  }
+}
