cfc7a5170bc697e1b9b9062f28b1796c183dc907
David Nicholas <Dnicholas@bitovi.com>
2022-10-05 09:34:42 -0700

    passHref

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -5,20 +5,20 @@ import styles from "./Nav.module.css";
 const Nav = () => {
   return (
     <nav className={styles.container}>
-      <Image src="/bitovi-red.png" alt="Bitovi Logo" width={100} height={30} />
+      <Image src="/bitovi-red.png" alt="Vercel Logo" width={100} height={30} />
       <ul className={styles.navList}>
         <li className={styles.navListItem}>
-          <Link href="/">
+          <Link href="/" passHref>
             <a className={styles.link}>Home</a>
           </Link>
         </li>
         <li className={styles.navListItem}>
-          <Link href="/cart?id=1">
+          <Link href="/cart?id=1" passHref>
             <a className={styles.link}>Cart</a>
           </Link>
         </li>
         <li className={styles.navListItem}>
-          <Link href="/about">
+          <Link href="/about" passHref>
             <a className={styles.link}>About</a>
           </Link>
         </li>
