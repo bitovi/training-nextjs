280483b2826c0ab9907381eca0832522c6148bf3
Andrew Coleburn <arcoleburn@gmail.com>
2022-10-19 19:01:47 -0400

    clean up

--- pages/catalog/[...catch-all].tsx
+++ pages/catalog/[...catch-all].tsx
@@ -1,8 +1,6 @@
 import CatalogCatchAll from "@scenes/Catalog/CatalogCatchAll";
 import { NextPage } from "next";
-import Link from "next/link";
 
-import styles from "./[...catch-all].module.css";
 
 const CatchAllPage: NextPage = () => {
   return (
